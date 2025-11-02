e6a7ccf4120a038e6438976913bc9fb245dd7d95
sanwalsulehri <sanwalsulehri077@gmail.com>
2025-11-02 19:28:08 +0500

    bn,m.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -8,9 +8,10 @@ interface ButtonProps {
   outline?: boolean;
   size?: 'sm' | 'md' | 'lg';
   disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export function Button({ children, onClick, className = "", bg = true, outline = false, size = 'md', disabled = false }: ButtonProps) {
+export function Button({ children, onClick, className = "", bg = true, outline = false, size = 'md', disabled = false, type = 'button' }: ButtonProps) {
   const sizeClasses = {
     sm: 'px-3 py-[7px] text-sm gap-1',
     md: 'px-4 py-2 text-sm gap-1.5',
@@ -38,6 +39,7 @@ export function Button({ children, onClick, className = "", bg = true, outline =
 
   return (
    <button 
+     type={type}
      onClick={onClick}
      disabled={disabled}
      className={`${sizeClasses[size]} flex font-medium items-center justify-center  cursor-pointer leading-tight rounded-lg font-medium transition-all duration-200 ease-in-out focus:outline-none ${defaultClasses} ${disabled ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''} ${className}`}
